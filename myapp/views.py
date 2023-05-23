f837d4ebea7703957569bf8b75775e946c294a85
sckimosu <sckim7@kookmin.ac.kr>
2023-05-23 19:03:33 +0900

    first_chaper

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
+
+
+# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class = PostSerializer
+
